4ddb7ff96a01551a5ec8437e82f2dc318326c9ac
Marcin Lisowski <marcin.lisowski91@outlook.com>
2023-10-21 19:02:23 +0200

    Zadanie dzień 4

--- DesignApp/Program.cs
+++ DesignApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string Name = "Ewa";
+string Sex = "kobieta";
+var Age = 23;
+
+if (Sex == "kobieta" && Age < 30) 
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (Name == "Ewa" && Age == 33 && Sex == "kobieta")
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (Sex == "mężczyzna" && Age < 18) 
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("Niepoprawne dane");   
+}
