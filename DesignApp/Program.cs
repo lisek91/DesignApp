4b32c882aed9fc6b7829daf34755b5ee8be77470
Marcin Lisowski <marcin.lisowski91@outlook.com>
2023-10-27 16:13:21 +0200

    Zadanie - klasy

--- DesignApp/Program.cs
+++ DesignApp/Program.cs
@@ -1,75 +1,51 @@
-﻿int number = 45975;
-string NumberAsString = number.ToString();
-char[] digits = NumberAsString.ToArray();
+﻿using DesignApp;
 
-int counter0 = 0;
-int counter1 = 0;
-int counter2 = 0;
-int counter3 = 0;
-int counter4 = 0;
-int counter5 = 0;
-int counter6 = 0;
-int counter7 = 0;
-int counter8 = 0;
-int counter9 = 0;
+Employee employee1 = new Employee("Adam", "Suś", 29);
+Employee employee2 = new Employee("Mariusz", "Sywak", 25);
+Employee employee3 = new Employee("Krzysztof", "Danielik", 27);
 
-foreach (char digit in digits) 
+employee1.Addscore(3);
+employee1.Addscore(7);
+employee1.Addscore(4);
+employee1.Addscore(6);
+employee1.Addscore(2);
+
+employee2.Addscore(2);
+employee2.Addscore(8);
+employee2.Addscore(6);
+employee2.Addscore(1);
+employee2.Addscore(5);
+
+employee3.Addscore(9);
+employee3.Addscore(6);
+employee3.Addscore(3);
+employee3.Addscore(7);
+employee3.Addscore(8);
+
+
+List<Employee> Employees = new List<Employee>()
 {
-    if (digit == '0')
+
+employee1, employee2, employee3
+
+};
+
+int maxResult = 0;
+Employee employeeWithMaxResult = null;
+
+foreach (var employee in Employees) 
+{
+
+    if (employee.Result > maxResult) 
     {
-        counter0++;
-    }
-    else if (digit == '1')
-    {
-        counter1++;
-    }
-    else if (digit == '2')
-    {
-        counter2++;
-    }
-    else if (digit == '3') 
-    {
-        counter3++;
-    }
-    else if (digit == '4') 
-    {
-        counter4++;
-    }
-    else if (digit == '5') 
-    {
-        counter5++;
-    }
-    else if (digit == '6') 
-    {
-        counter6++;
-    }
-    else if (digit == '7') 
-    {
-        counter7++;
-    }
-    else if (digit == '8') 
-    {
-        counter8++;
-    }
-    else if (digit == '9') 
-    {
-        counter9++;
+
+        employeeWithMaxResult = employee;
+            
     }
 
 }
 
-List<string> dig = new List<string>();
+Console.WriteLine(employeeWithMaxResult.Name + " " + employeeWithMaxResult.Surname + " " + "w wieku" + " " + employeeWithMaxResult.Age + " " + "lat");
+Console.WriteLine("otrzymał wynik" + " " + employeeWithMaxResult.Result + " ");
 
-dig.Add("0=>" + counter0);
-dig.Add("1=>" + counter1);
-dig.Add("2=>" + counter2);
-dig.Add("3=>" + counter3);
-dig.Add("4=>" + counter4);
-dig.Add("5=>" + counter5);
-dig.Add("6=>" + counter6);
-dig.Add("7=>" + counter7);
-dig.Add("8=>" + counter8);
-dig.Add("9=>" + counter9);
 
-foreach (var counter in dig)
-    Console.WriteLine(counter);
---
+++ DesignApp/User.cs
@@ -0,0 +1,42 @@
+﻿
+
+namespace DesignApp
+
+{
+    class Employee
+    {
+        private List<int> score = new List<int>();
+
+        public Employee(string name, string surname, int age)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+
+        }
+
+        public string Name { get; private set; }
+
+        public string Surname { get; private set; }
+
+        public int Age { get; private set; }
+
+        public int Result
+        {
+            get
+            {
+                return score.Sum();
+            }
+
+        }
+
+        public void Addscore(int number)
+        {
+            this.score.Add(number);
+        }
+
+
+    }
+
+}
+
